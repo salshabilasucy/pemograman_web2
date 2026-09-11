f16d404bc5263defbaf8bab54ccaf46c5e43df86
Salshabilasucy <salshabilasucy@gmail.com>
2026-09-11 20:08:35 +0700

    Add files via upload

---
+++ latihan4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan 4 - Jadwal</title>
+</head>
+<body>
+<?php
+$day = "Wednesday"; // Kamu bisa ganti variabel hari ini sesuai keinginan
+
+switch ($day) {
+    case "Wednesday":
+        Print("Rabu <br>");
+        print ("Seminar Launching Window Vista di JHCC");
+        Break;
+    case "Thrusday":
+        Print("Kamis <br>");
+        print "Pertemuan dengan Mahasiswa";
+        Break;
+    case "Friday":
+        Print("Jum'at <br>");
+        print "Jogging bersama";
+        Break;
+    Default:
+        Print("Sabtu <br>");
+        print "Survey harga ke Dusit, Mangga Dua";
+}
+?>
+</body>
+</html>
